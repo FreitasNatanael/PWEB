cccff949a0c33c2337d76f95da90cab4ca6984ef
FreitasNatanael <nfreitas738@gmail.com>
2026-04-08 20:56:45 -0300

    Add files via upload

---
+++ Atividade6/baseMedia.js
@@ -0,0 +1,10 @@
+nome=(prompt("nome do aluno"));
+nota1=parseFloat(prompt("nota 1:"));
+nota2=parseFloat(prompt("nota 2:"));
+nota3=parseFloat(prompt("nota 3:"));
+nota4=parseFloat(prompt("nota 4:"));
+media= (nota1+nota2+nota3+nota4) / 4;
+
+alert("Nome do aluno: " + nome +
+    "\n Media do aluno: " + media);
+
