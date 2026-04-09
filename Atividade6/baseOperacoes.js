6212f19b80675986b42aca11c66f4bc9478b94da
FreitasNatanael <nfreitas738@gmail.com>
2026-04-08 21:08:31 -0300

    Add files via upload

---
+++ Atividade6/baseOperacoes.js
@@ -0,0 +1,12 @@
+
+numero1=parseFloat(prompt("numero 1:"));
+numero2=parseFloat(prompt("numero 2:"));
+
+
+
+alert(" Soma: " + (numero1+numero2) +
+    "\n Subtração primeiro pelo segundo: " + (numero1 - numero2) +
+    "\n Produto: " + (numero1*numero2) +
+    "\n Divisão do primeiro pelo segundo: " + (numero1 / numero2).toFixed(2) +
+    "\n Resto da divisão do primeiro pelo segundo: " + (numero1 % numero2));
+
