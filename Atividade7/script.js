5824151d175acf9e20404c802de1fe6a8e8c11cd
FreitasNatanael <nfreitas738@gmail.com>
2026-04-15 20:33:29 -0300

    Add files via upload

---
+++ Atividade7/script.js
@@ -0,0 +1,37 @@
+while (true){
+Jogador = (prompt("Insira sua opção: \n1 - Pedra \n2 - Papel \n3 - Tesoura"));
+
+PC = Math.random().toFixed(2);
+
+if (PC<=0.33 && Jogador==1){
+    alert("O computador escolheu pedra. O resultado é empate.");
+}
+
+if (PC<=0.33 && Jogador==2){
+    alert("O computador escolheu pedra. Você ganhou :)");
+}
+
+if (PC<=0.33 && Jogador==3){
+    alert("O computador escolheu pedra. Você perdeu :(");
+}
+
+if (PC>0.34  && PC<=0.67 && Jogador==1){
+    alert("O computador escolheu Papel. Você perdeu :(");
+}
+if (PC>0.34  && PC<=0.67 && Jogador==2){
+    alert("O computador escolheu Papel. O resultado é empate.");
+}
+if (PC>0.34  && PC<=0.67 && Jogador==3){
+    alert("O computador escolheu Papel. Você ganhou :)");
+}
+
+if (PC>0.67  && Jogador==1){
+    alert("O computador escolheu Tesoura. Você ganhou :) ");
+}
+if (PC>0.67  && Jogador==2){
+    alert("O computador escolheu Tesoura. Você perdeu :(");
+}
+if (PC>0.67  && Jogador==3){
+    alert("O computador escolheu Tesoura. O resultado é empate.");
+}
+}
