e45687cf16f8682171fa812058affa2098532819
FreitasNatanael <nfreitas738@gmail.com>
2026-04-15 20:48:51 -0300

    Add files via upload

--- Atividade7/script.js
+++ Atividade7/script.js
@@ -15,13 +15,13 @@ if (PC<=0.33 && Jogador==3){
     alert("O computador escolheu pedra. Você perdeu :(");
 }
 
-if (PC>0.34  && PC<=0.67 && Jogador==1){
+if (PC>0.33  && PC<=0.67 && Jogador==1){
     alert("O computador escolheu Papel. Você perdeu :(");
 }
-if (PC>0.34  && PC<=0.67 && Jogador==2){
+if (PC>0.33  && PC<=0.67 && Jogador==2){
     alert("O computador escolheu Papel. O resultado é empate.");
 }
-if (PC>0.34  && PC<=0.67 && Jogador==3){
+if (PC>0.33  && PC<=0.67 && Jogador==3){
     alert("O computador escolheu Papel. Você ganhou :)");
 }
 
